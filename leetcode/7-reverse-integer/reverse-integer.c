9e3b4e85feb964a7a7bb1adb86720a227a3ee44d
gmgass <gmgass@inf.ufpel.edu.br>
2025-05-29 02:57:30 -0300

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.36%) - LeetSync

--- leetcode/7-reverse-integer/reverse-integer.c
+++ leetcode/7-reverse-integer/reverse-integer.c
@@ -1,15 +1,14 @@
 int reverse(int x){
-    int ans = 0;
+    long int ans = 0;
 
     while ( x != 0 ) {
-        int digit = x % 10;
+        ans = ans * 10 + ( x % 10 );
         x /= 10;
-
-        if ( ans > INT_MAX / 10 || ( ans == INT_MAX / 10 && digit > 7 )) return 0;
-        if ( ans < INT_MIN / 10 || ( ans == INT_MIN / 10 && digit < -8)) return 0;
-
-        ans = ans * 10 + digit;
     }
 
-    return ans;
+    if ( ans < INT_MIN || ans > INT_MAX ) {
+        return 0;
+    }
+
+    return ( int ) ans;
 }
