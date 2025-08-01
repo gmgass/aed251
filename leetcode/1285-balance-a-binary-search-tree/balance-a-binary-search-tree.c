6b01e538da0100cbb8be4fec78cb50ac1a1583fb
gmgass <gmgass@inf.ufpel.edu.br>
2025-08-01 18:57:45 -0300

    Time: 11 ms (50.00%) | Memory: 44.4 MB (42.19%) - LeetSync

--- leetcode/1285-balance-a-binary-search-tree/balance-a-binary-search-tree.c
+++ leetcode/1285-balance-a-binary-search-tree/balance-a-binary-search-tree.c
@@ -1,10 +1,3 @@
-/* struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
-
 int countNodes ( struct TreeNode *root ) {
     if ( !root ) return 0;
     return 1 + countNodes( root->left ) + countNodes( root ->right );
@@ -21,28 +14,25 @@ void inOrder ( struct TreeNode *root, int *arr, int *i ) {
 struct TreeNode *arrayToBST ( int *arr, int start, int end ) {
     if ( start > end ) return NULL; 
     
-    int mid = start + ( end - start ) / 2; // "mid = (start + end) / 2" pode causar overflow.
+    int mid = start + ( end - start ) / 2;
 
     struct TreeNode *node = ( struct TreeNode* ) malloc( sizeof( struct TreeNode ));
     node->val = arr[ mid ];
-    node->left = arrayToBST( arr, start, mid - 1 ); // 30min preso num stack overflow porque esqueci do "-1".
+    node->left = arrayToBST( arr, start, mid - 1 );
     node->right = arrayToBST ( arr, mid + 1, end );
     
     return node;
 }
 
 struct TreeNode *balanceBST( struct TreeNode *root ) {
-    if ( !root ) return NULL; // Tinha esquecido dessa verificação.
+    if ( !root ) return NULL;
 
-    // Primeiro conto a quantidade de nós na árvore para criar um vetor de mesmo tamanho.
     int arrSize = countNodes ( root );
     int *arr = ( int* ) malloc( sizeof( int ) * arrSize );
-    int i = 0; // Não posso declarar dentro de uma função recursiva.
+    int i = 0;
 
-    // Em seguida, preencho o vetor de forma ordenada.
-    inOrder( root, arr, &i ); // Tinha esquecido de passar o arr como parametro e de colocar o &.
+    inOrder( root, arr, &i );
 
-    // Por fim, construo uma nova árvore balanceada a partir do vetor ordenado.
     struct TreeNode *newRoot;
     newRoot = arrayToBST ( arr, 0, arrSize - 1 );
 
