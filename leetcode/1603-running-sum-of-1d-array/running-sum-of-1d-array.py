47955696ff1ddb2b292c603f692d58b06e6760dc
gmgass <gmgass@inf.ufpel.edu.br>
2025-09-10 10:11:02 -0300

    Time: 0 ms (100.00%) | Memory: 12.7 MB (32.30%) - LeetSync

---
+++ leetcode/1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = [nums[0]]
+
+        for i in range( 1, len(nums) ):
+            result.append( result[i - 1] + nums[i] )
+
+        return result
